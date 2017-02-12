193ffa5a5ebc964fb7c97cb41641b74febd61a63
milanchheda <milan.chheda@gmail.com>
2017-02-12 23:37:35 +0530

    Added GA code.

--- index.php
+++ index.php
@@ -20,6 +20,15 @@ unset($_SESSION['teamID']);
     <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js"></script> -->
     <!-- <script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/af-2.1.3/fc-3.2.2/fh-3.1.2/kt-2.2.0/r-2.1.0/se-1.2.0/datatables.min.js"></script> -->
     <!-- <script type="text/javascript" src="js/fpl.js"></script> -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-91316667-2', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
 <body>
     <div class="container" id="container" data-page="">
