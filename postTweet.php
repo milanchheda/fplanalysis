58ed40b82809c908ade78dd1d3eaa8c4809e8002
Ubuntu <ubuntu@ip-172-31-19-79.ap-southeast-1.compute.internal>
2017-02-24 05:04:58 +0000

    Updated

--- postTweet.php
+++ postTweet.php
@@ -1,38 +1,40 @@
 <?php
 // Create app and add keys from 
 // https://apps.twitter.com/
+require_once __DIR__ . "/config.php";
+require_once __DIR__ . "/twitteroauth.php";
 
 // API OAuth 
 // You need add files: 
 // OAuth.php 
 // twitteroauth.php  
 // and keys $consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret
-require_once('twitteroauth.php');
-require('config.php');
+//require_once('twitteroauth.php');
+//require('config.php');
 
 $random = rand(1, 6);
 
 switch ($random) {
 	case 1:
-		$msg = getTopThreeSelectedByPlayers();
+		$msg = getTopThreeSelectedByPlayers($conn);
 		break;
 	case 2:
-		$msg = getTopTransferredInPlayer();
+		$msg = getTopTransferredInPlayer($conn);
 		break;
 	case 3:
-		$msg = getTopTransferredOutPlayer();
+		$msg = getTopTransferredOutPlayer($conn);
 		break;
 	case 4:
-		$msg = getTopThreePlayersInForm();
+		$msg = getTopThreePlayersInForm($conn);
 		break;
 	case 5:
-		$msg = getTopThreeGoalScorers();
+		$msg = getTopThreeGoalScorers($conn);
 		break;
 	case 6:
-		$msg = getTopThreeAssists();
+		$msg = getTopThreeAssists($conn);
 		break;
 	default:
-		$msg = getTopThreeSelectedByPlayers();
+		$msg = getTopThreeSelectedByPlayers($conn);
 		break;
 }
 
@@ -44,7 +46,7 @@ if(strlen($msg) < 140) {
 }
 
 
-function getTopThreeSelectedByPlayers() {
+function getTopThreeSelectedByPlayers($conn) {
 	$getTopThreeSelectedBy = mysqli_query($conn, "SELECT web_name, selected_by FROM players ORDER BY selected_by DESC limit 3");
 	while($result = mysqli_fetch_array($getTopThreeSelectedBy)) {
 		$players[] = $result['selected_by'] . "% — " . "#" . $result['web_name'];
@@ -53,7 +55,7 @@ function getTopThreeSelectedByPlayers() {
 	return $msg;
 }
 
-function getTopTransferredInPlayer() {
+function getTopTransferredInPlayer($conn) {
 	$getTopThreeSelectedBy = mysqli_query($conn, "SELECT web_name, transfers_in_event FROM players ORDER BY transfers_in_event DESC limit 1");
 	while($result = mysqli_fetch_array($getTopThreeSelectedBy)) {
 		$playerName = "#" . $result['web_name'];
@@ -63,7 +65,7 @@ function getTopTransferredInPlayer() {
 	return $msg;
 }
 
-function getTopTransferredOutPlayer() {
+function getTopTransferredOutPlayer($conn) {
 	$getTopThreeSelectedBy = mysqli_query($conn, "SELECT web_name FROM players ORDER BY transfers_out_event DESC limit 3");
 	while($result = mysqli_fetch_array($getTopThreeSelectedBy)) {
 		$playerName[] = "#" . $result['web_name'];
@@ -72,7 +74,7 @@ function getTopTransferredOutPlayer() {
 	return $msg;
 }
 
-function getTopThreePlayersInForm() {
+function getTopThreePlayersInForm($conn) {
 	$getTopThreeSelectedBy = mysqli_query($conn, "SELECT web_name, form FROM players where status = 'a' ORDER BY transfers_in_event DESC limit 3");
 	while($result = mysqli_fetch_array($getTopThreeSelectedBy)) {
 		$players[] = "#" . $result['web_name'] . " — " . $result['form'];
@@ -81,7 +83,7 @@ function getTopThreePlayersInForm() {
 	return $msg;
 }
 
-function getTopThreeGoalScorers() {
+function getTopThreeGoalScorers($conn) {
 	$getTopThreeSelectedBy = mysqli_query($conn, "SELECT web_name, goals_scored FROM players where status = 'a' ORDER BY goals_scored DESC limit 3");
 	while($result = mysqli_fetch_array($getTopThreeSelectedBy)) {
 		$players[] = "#" . $result['web_name'] . " — " . $result['goals_scored'];
@@ -90,7 +92,7 @@ function getTopThreeGoalScorers() {
 	return $msg;
 }
 
-function getTopThreeAssists() {
+function getTopThreeAssists($conn) {
 	$getTopThreeSelectedBy = mysqli_query($conn, "SELECT web_name, assists FROM players where status = 'a' ORDER BY assists DESC limit 3");
 	while($result = mysqli_fetch_array($getTopThreeSelectedBy)) {
 		$players[] = "#" . $result['web_name'] . " — " . $result['assists'];
